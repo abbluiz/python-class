5a53715329c39754bb0656abe633c4d49a04ee5b
abbluiz <unt@labb.email>
2019-11-06 10:47:29 -0500

    Add Lab 9

---
+++ lab-9.py
@@ -0,0 +1,89 @@
+# 1
+# Answer: Programming language
+
+# 2
+# Answer: B
+
+# 3
+print("Hi")
+
+# 4
+# Answer: quit()
+
+# 5
+# Answer: 6
+
+# 6
+# Answer: B
+
+# 7
+# Answer: - 10 +
+
+# 8
+# Answer: >>> 1 / 0
+
+# 9
+# Answer: A
+
+# 10
+# Answer: 15.0
+
+# 11
+# Answer: 3
+
+# 12
+# Answer: A
+
+# 13
+# Answer: \"something\"
+
+# 14
+# Answer: input
+
+# 15
+# Answer: "World"
+
+# 16
+# Answer: D
+
+# 17
+# Answer: 777
+
+# 18
+# Answer: A
+
+# 19
+# Answer: A
+
+# 20
+# Answer: B
+
+# 21
+# Answer: 7
+
+# 22
+# Answer: A
+
+# 23
+# Answer: 20
+
+# 24
+# Answer: 12
+
+# 25
+# Answer: aaa
+
+# 26
+# Answer: A
+
+# 27
+# Answer: C
+
+# 28
+# Answer: B
+
+# 29
+# Answer: 82
+
+# 30
+# Answer: 2
