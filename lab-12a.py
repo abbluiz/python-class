85734c7c12c389868e26d26a9509fb0437c843ff
abbluiz <unt@labb.email>
2019-11-06 11:39:00 -0500

    Start Lab 12

---
+++ lab-12a.py
@@ -0,0 +1,16 @@
+# 1
+
+def inputStudentName():
+    return input("Enter your name: ")
+
+def inputStudentMidtermGrade():
+    return float(input("Enter your midterm grade: "))
+
+def inputStudentFinalGrade():
+    return float(input("Enter your final grade: "))
+
+def inputStudentAssignmentGrade():
+    return float(input("Enter your assignment grade: "))
+
+def printHeader():
+    print("Type exit to quit the program.")
